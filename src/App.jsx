aab069c4373ed1a1a338a9246dcd996bc84400ae
Rachna Singh <rachnasningh092003@gmail.com>
2024-04-01 17:47:23 +0530

    making sqaure board . style the board, display the next player information and also display the winner and tied

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
-import Board from './components/Board';
 import './style.scss';
+import { useState } from 'react';
+import Board from './components/Board';
+import StatusMessage from './components/StatusMessage';
+import { calculateWinner } from './winner';
 
 function App() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(squares);
+
+  console.log(squares);
+  const handleSquareClick = clickedPosition => {
+    if (squares[clickedPosition] || winner) {
+      return;
+    }
+
+    setSquares(currentSquares => {
+      return currentSquares.map((squareValue, position) => {
+        if (clickedPosition === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return squareValue;
+      });
+    });
+    setIsXNext(currentIsXNext => !currentIsXNext);
+  };
+
   return (
     <div className="app">
-      <Board />
+      <StatusMessage winner={winner} isXNext={isXNext} squares={squares} />
+      <Board squares={squares} handleSquareClick={handleSquareClick} />
     </div>
   );
 }
