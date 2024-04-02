2311f06aa31abb6eaa90d064641eefadd6ef7128
Rachna Singh <rachnasningh092003@gmail.com>
2024-04-02 15:23:30 +0530

    add the final touch in background

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,9 @@ function App() {
 
   return (
     <div className="app">
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
       <StatusMessage winner={winner} gamingBoard={gamingBoard} />
       <Board
         squares={gamingBoard.squares}
@@ -72,9 +75,10 @@ function App() {
         Start New Game
       </button>
 
-      <h1>Current History</h1>
+      <h2 style={{ fontWeight: 'normal' }}>Current Game History</h2>
 
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls" />
     </div>
   );
 }
